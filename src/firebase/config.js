552fd9832db44d6be963aaabad7d5b6930c953be
Harmandeepsinghteja <harmandeepsinghteja@gmail.com>
2025-02-28 11:00:50 -0700

    Logout added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -15,5 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
+const firebase_app = initializeApp(firebaseConfig);
+export default firebase_app;
