09b3b59c35d44998e1868b8b54872c3fb30bf812
Pahul Brar <pahul.brar@gettyimages.com>
2025-02-28 22:53:42 -0700

    Added a Review Button

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -11,7 +11,7 @@ export default function Sidebar(){
             <div className="items-center justify-start max-w-md w-full px-6 py-8 bg-white rounded-lg shadow-md">
                 <h1 className="text-gray-900 text-3xl  font-bold text-center mb-6">What would you like to do?</h1>
                 
-                <ul className='flex justify-center space-x-3'>
+                <ul className='flex justify-center space-x-2'>
                 <li>
                         <button onClick={() => router.push("/questionnaire")}
                              className="text-green-500 bg-transparent border-2 border-green-500 rounded-md px-4 py-2 hover:bg-green-500 hover:text-white transition-colors">
